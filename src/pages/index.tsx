c86b54e92bd2560bf09f89e3ce5d128a653ddcf7
Alex Welcing <alexwelcing@gmail.com>
2021-08-10 17:13:33 -0400

    add updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Thanks for visiting. <span className="fancy">I'm an Alex.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@alexwelcing</span>
+          <h2>A web person working on a better web for people.</h2>
           <SocialList />
         </div>
       </div>
